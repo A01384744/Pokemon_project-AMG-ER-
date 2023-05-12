bc60ab42cfed669b7d73c841e1e9762f60dfbaf9
A01384744 <102888149+A01384744@users.noreply.github.com>
2023-05-12 10:44:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,153 @@
+import requests
+import random
+
+#1 Fetch Data
+
+# ask user to enter the pokemon and stores it as pokemon_name.
+pokemon1_name = input("Enter the name of a Pokémon (without caps): ")
+print('--------------------------------------------------------------------------------------------')
+
+# url to acces the pokemon data, f is to be able to insert the pokemon_name to the url
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon1_name}"
+print(f'{pokemon1_name} stats:')
+# Send a GET(get the info) request to the PokeAPI using the URL, and store the response in the "response" variable.
+response = requests.get(url)
+
+# Check if the response status code is equal to 200, 200=sucseful.
+if response.status_code == 200:
+    # since the PokeApi send its info in JSON we change it to Python object stored it in the "data" variable.
+    data = response.json()
+    # Extract the HP (health points) of the Pokémon from the "data" dictionary by accessing the first element of the "stats" list and then the "base_stat" key.
+    hp = data["stats"][0]["base_stat"]
+    # Extract the weight of the Pokémon from the "data" dictionary by accessing the "weight" key.
+    weight = data["weight"]
+    # Print out the name of the Pokémon entered by the user along with its HP and weight using an f-string.
+    print(f"{pokemon1_name}'s HP is {hp} and its weight is {weight}.")
+else:
+    # If the response status code is not equal to 200, print an error message.
+    print("Could not retrieve information about the Pokémon.")
+
+#2b moves
+
+# url to access the pokemon data, f is to be able to insert the pokemon_name to the url
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon1_name}"
+
+# Send a GET(get the info) request to the PokeAPI using the URL, and store the response in the "response" variable.
+response = requests.get(url)
+
+# Check if the response status code is equal to 200, 200=sucseful.
+if response.status_code == 200:
+    # since the PokeApi send its info in JSON we change it to Python object stored it in the "data" variable.
+    data = response.json()
+    # Extract the HP (health points) of the Pokémon from the "data" dictionary by accessing the first element of the "stats" list and then the "base_stat" key.
+    hp = data["stats"][0]["base_stat"]
+    # Extract the weight of the Pokémon from the "data" dictionary by accessing the "weight" key.
+    weight = data["weight"]
+    # Extract the moves of the Pokémon from the "data" dictionary by accessing the "moves" list and then the "name" key.}
+    moves_list = []
+    for move in data['moves'][:5]:
+        move_name = move['move']['name']
+        moves_list.append(move_name)
+
+    # Print out the name of the Pokémon entered by the user along with its HP, weight and moves using an f-string.
+    print(f"{pokemon1_name}'s moves are: {', '.join(moves_list)}.")
+else:
+    # If the response status code is not equal to 200, print an error message.
+    print("Could not get information of the Pokémon.")
+
+
+
+print('-----------------------------------------------------------------------------------------------------')
+
+#2a Count 
+print('Pokemon count')
+# Send a GET request to the Pokemon API's Pokemon resource and store the response in the "response" variable
+response = requests.get("https://pokeapi.co/api/v2/pokemon/")
+# Check if the response status code is equal to 200 (successful)
+if response.status_code == 200:
+    # Convert the response to a Python object and extract the "count" attribute, which tells us how many Pokemon exist
+    data = response.json()
+    count = data["count"]
+    print(f"There are {count} Pokémon in total.")
+else:
+    # If the response status code is not equal to 200, print an error message.
+    print("Could not retrieve information about the Pokémon.")
+
+print('----------------------------------------------------------------------------------------------')
+
+#2c Healthier
+print('HP fight')
+# Get a random Pokemon name
+random_pokemon_id = random.randint(1, count)
+
+url = f"https://pokeapi.co/api/v2/pokemon/{random_pokemon_id}"
+response = requests.get(url)
+
+# Check if the response status code is equal to 200, 200=sucseful.
+if response.status_code == 200:
+    data = response.json()
+    pokemon_name = data["name"] 
+    # Get a random Pokémon from the list
+    random_pokemon = random.choice(data["stats"])
+    random_pokemon_hp = random_pokemon["base_stat"]
+    
+    print(f"The random Pokémon is: {pokemon_name}")
+    print(f"{pokemon_name} hp is: {random_pokemon_hp}, and {pokemon1_name} hp is: {hp}")
+
+    if random_pokemon_hp > hp:
+        print(f"{pokemon_name} is healthier than {pokemon1_name}")
+    else:
+      if hp>random_pokemon_hp:
+       print(f"{pokemon1_name} is healthier than {pokemon_name}!")
+      else:
+         print(f"both {pokemon1_name} and {pokemon_name} have the same hp {hp}")
+else:
+    print("Could not retrieve information about the Pokémon.")
+
+print('--------------------------------------------------------------------------------------')
+
+
+#3a how many pokemons weight more
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon1_name}"
+response = requests.get(url)
+
+# Check if the response status code is equal to 200, 200=successful.
+if response.status_code == 200:
+    data = response.json()
+    pokemon1_weight = data["weight"]
+    
+    print(f"{pokemon1_name}'s weight is: {pokemon1_weight}")
+    print(f"checking for how many pokemons are heavier than {pokemon1_name}, please wait a minute :)")
+    
+    offset = 0
+    limit = count
+    more_pokemons = True
+
+    while more_pokemons:
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/?offset={offset}&limit={limit}")
+        if response.status_code == 200:
+            pokemon_list_data = response.json()
+            pokemon_list = pokemon_list_data["results"]
+            count_heavier = 0
+
+            for pokemon in pokemon_list:
+                response = requests.get(pokemon["url"])
+                if response.status_code == 200:
+                    pokemon_data = response.json()
+                    pokemon_name = pokemon_data["name"]
+                    pokemon_weight = pokemon_data["weight"]
+                    
+                    if pokemon_weight > pokemon1_weight:
+                        count_heavier = count_heavier + 1
+                else:
+                    print(f"Failed to retrieve data for {pokemon['name']}")
+
+            offset = offset + limit
+
+            if offset >= pokemon_list_data["count"]:
+                more_pokemons = False
+        else:
+            print("Failed to retrieve Pokémon list")
+    print(f'In total there are {count_heavier} pokemons heavier than {pokemon1_name}')
+else:
+    print("Could not retrieve information about the Pokémon.")
